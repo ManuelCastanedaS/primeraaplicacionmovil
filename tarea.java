adeb47e1d916c787c903ec491f71f2299eb3ba6f
ManuelCastanedaS <111100887+ManuelCastanedaS@users.noreply.github.com>
2024-04-24 22:25:51 -0400

    Add files via upload

--- tarea.java
+++ tarea.java
@@ -5,23 +5,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Solicitar datos al usuario
-        System.out.print("Ingrese la marca del vehículo: ");
-        String marca = scanner.nextLine();
+        System.out.print("Ingrese la marca del vehículo: "); //muestra por pantalla solicitud
+        String marca = scanner.nextLine(); // recibe el dato del usuario y lo guarda en una variable
 
-        System.out.print("Ingrese el modelo del vehículo: ");
-        String modelo = scanner.nextLine();
+        System.out.print("Ingrese el modelo del vehículo: "); //muestra por pantalla solicitud
+        String modelo = scanner.nextLine(); // recibe el dato del usuario y lo guarda en una variable
 
-        System.out.print("Ingrese la cilindrada del vehículo: ");
-        int cilindrada = scanner.nextInt();
+        System.out.print("Ingrese la cilindrada del vehículo: "); //muestra por pantalla solicitud
+        int cilindrada = scanner.nextInt(); // recibe el dato del usuario y lo guarda en una variable
         scanner.nextLine(); // Consumir el salto de línea
 
-        System.out.print("Ingrese el tipo de combustible del vehículo: ");
-        String tipoCombustible = scanner.nextLine();
+        System.out.print("Ingrese el tipo de combustible del vehículo: "); //muestra por pantalla solicitud
+        String tipoCombustible = scanner.nextLine(); // recibe el dato del usuario y lo guarda en una variable
 
-        System.out.print("Ingrese la capacidad en pasajeros del vehículo: ");
-        int capacidadPasajeros = scanner.nextInt();
+        System.out.print("Ingrese la capacidad en pasajeros del vehículo: "); //muestra por pantalla solicitud
+        int capacidadPasajeros = scanner.nextInt(); // recibe el dato del usuario y lo guarda en una variable
 
-        // Mostrar los resultados
+        // Mostrar los resultados guardados anteriormente en cada variable
         System.out.println("La marca que ha ingresado es: " + marca);
         System.out.println("El modelo que ha ingresado es: " + modelo);
         System.out.println("La cilindrada que ha ingresado es: " + cilindrada);
